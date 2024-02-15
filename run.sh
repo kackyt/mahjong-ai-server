bcc16ffe2367e523e809b76d910c518d58c9bfc6
kacky <t_kaki@nextappli.com>
2024-02-15 22:24:43 +0900

    feat: envを使ってファイルをダウンロードして結果をアップロード

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./server -i target.dll -l ./logs -p ./pailist
+gcloud storage cp ${AI_DOWNLOAD_URL} ./target.dll
+gcloud storage cp ${PAIYAMA_DOWNLOAD_URL} ./paiyama --recursive
+
+./server -i target.dll -l ./logs -p ./paiyama
+
+gcloud storage cp ./logs ${LOGS_UPLOAD_URL} --recursive
