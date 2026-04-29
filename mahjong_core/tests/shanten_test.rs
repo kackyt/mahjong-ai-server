9f0932cb359c14cfcd6b28fc0eae4c5b7aecd94c
kackyt <t_kaki@nextappli.com>
2026-04-29 14:35:39 +0900

    fix: cargo fmt

--- mahjong_core/tests/shanten_test.rs
+++ mahjong_core/tests/shanten_test.rs
@@ -1,4 +1,4 @@
-﻿use mahjong_core::{mahjong_generated::open_mahjong::PaiT, shanten::PaiState};
+use mahjong_core::{mahjong_generated::open_mahjong::PaiT, shanten::PaiState};
 use std::{
     fs::File,
     io::{self, BufRead, BufReader, Error},
@@ -94,4 +94,3 @@ fn calc_tin_shanten_test() -> Result<(), Error> {
 
     Ok(())
 }
-
